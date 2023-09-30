a072d16b9e902004543856958f3514968bbbeb83
alexander <alexandernyaga1999@gmail.com>
2023-09-30 18:46:42 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Code your solution here
+function findMatching(drivers, name) {
+    const lowercaseName = name.toLowerCase();
+    const matchingDrivers = drivers.filter(driver => driver.toLowerCase() === lowercaseName);
+    return matchingDrivers;
+  }
+  const drivers = ['Bobby', 'Samantha', 'bobby', 'Tom'];
+  
+  const result = findMatching(drivers, 'Bobby');
+  console.log(result);
+
+  function fuzzyMatch(drivers, query) {
+    const matchingDrivers = drivers.filter(driver => driver.startsWith(query));
+  
+    return matchingDrivers;
+  }
+  
+  const driver = ['Sammy', 'Samantha', 'Sarah', 'Sally'];
+  
+  const results = fuzzyMatch(drivers, 'Sa');
+  console.log(result);
+
+  function matchName(drivers, nameToMatch) {
+    const matchingDrivers = drivers.filter(driver => {
+      return driver.name === nameToMatch;
+    });
+  
+    return matchingDrivers;
+  }
+  
+  
+    
+  
+  
